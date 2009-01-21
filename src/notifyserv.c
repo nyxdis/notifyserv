4310d865f3907ea347e97b3c04383190f33d9a07
Christoph Mende <angelos@gentoo.org>
2009-01-21 23:49:01 +0100

    Updated Copyright year

--- src/notifyserv.c
+++ src/notifyserv.c
@@ -1,7 +1,7 @@
 /*
  * IRC notification system
  *
- * Copyright (c) 2008, Christoph Mende <angelos@unkreativ.org>
+ * Copyright (c) 2008-2009, Christoph Mende <angelos@unkreativ.org>
  * All rights reserved. Released under the 2-clause BSD license.
  */
 
